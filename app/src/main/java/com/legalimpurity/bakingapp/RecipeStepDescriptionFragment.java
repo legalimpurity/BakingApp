ea9a39d560ce412b105800dbcb503ae5a6e319a9
rajatkhanna <legalimpurity@gmail.com>
2017-08-22 23:18:56 +0530

    exoplayer and image view in sync, now need to fix the potrait mode view for video player.

--- app/src/main/java/com/legalimpurity/bakingapp/RecipeStepDescriptionFragment.java
+++ app/src/main/java/com/legalimpurity/bakingapp/RecipeStepDescriptionFragment.java
@@ -1,22 +1,59 @@
 package com.legalimpurity.bakingapp;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.legalimpurity.bakingapp.Utils.UrlUtils;
 import com.legalimpurity.bakingapp.objects.Step;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 
-public class RecipeStepDescriptionFragment extends Fragment {
+public class RecipeStepDescriptionFragment extends Fragment{
 
     public static final String ARG_STEP_OBJ = "ARG_STEP_OBJ";
 
     private Step step;
+
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+
+    private ImageView imageView;
 
     public RecipeStepDescriptionFragment() {
     }
@@ -34,6 +71,13 @@ public class RecipeStepDescriptionFragment extends Fragment {
                 appBarLayout.setTitle(step.getShortDescription());
             }
         }
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
     }
 
     @Override
@@ -41,10 +85,101 @@ public class RecipeStepDescriptionFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.recipe_detail, container, false);
 
+        TextView recipe_detail = (TextView) rootView.findViewById(R.id.recipe_detail);
         if (step != null) {
-            ((TextView) rootView.findViewById(R.id.recipe_detail)).setText(step.getDescription());
+            recipe_detail.setText(step.getDescription());
         }
 
+        mPlayerView = rootView.findViewById(R.id.recipePlayerView);
+        imageView = rootView.findViewById(R.id.recipeImageView);
+
+        if(!TextUtils.isEmpty(step.getVideoURL()))
+        {
+            Uri.Builder b = Uri.parse(step.getVideoURL()).buildUpon();
+            initializePlayer(getActivity(),b.build());
+            mPlayerView.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.GONE);
+
+        }
+        else if(!TextUtils.isEmpty(step.getThumbnailURL()))
+        {
+            Picasso.with(getActivity())
+                    .load(step.getThumbnailURL())
+                    .into(imageView, new Callback() {
+                        @Override
+                        public void onSuccess() {
+
+                        }
+
+                        @Override
+                        public void onError() {
+
+                        }
+                    });
+            mPlayerView.setVisibility(View.GONE);
+            imageView.setVisibility(View.VISIBLE);
+
+        }
+        else
+        {
+            recipe_detail.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,1f));
+        }
         return rootView;
+    }
+
+
+    private void initializePlayer(FragmentActivity act, Uri mediaUri) {
+        if (mExoPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(act, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            mExoPlayer.addListener(new ExoPlayer.EventListener() {
+                @Override
+                public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+                }
+
+                @Override
+                public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+                }
+
+                @Override
+                public void onLoadingChanged(boolean isLoading) {
+
+                }
+
+                @Override
+                public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+                }
+
+                @Override
+                public void onPlayerError(ExoPlaybackException error) {
+
+                }
+
+                @Override
+                public void onPositionDiscontinuity() {
+
+                }
+            });
+
+            String userAgent = Util.getUserAgent(act, "ClassicalMusicQuiz");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    act, userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        if(mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
     }
 }
